5d2de9e3987d50b8bde50a499f465415f04e2527
Mark Lorens <marklorens0705@gmail.com>
2026-04-17 16:19:15 +0800

    Fix hehe sorry

--- Untilled/Model/PlantData.swift
+++ Untilled/Model/PlantData.swift
@@ -8,9 +8,8 @@
 import SwiftData
 import Foundation
 
-@Model
-class PlantData{
-    @Attribute(.unique) var ID: UUID
+struct PlantData{
+    let ID = UUID()
     var plantName: String
     var plantType: String
     var minTemperature: Double
@@ -22,17 +21,4 @@ class PlantData{
     var minUV: Int
     var maxUV: Int
     
-    init(plantName: String, plantType: String, minTemperature: Double, maxTemperature: Double, minHumidity: Double, maxHumidity: Double, minPrecipitation: Double, maxPrecipitation: Double, minUV: Int, maxUV: Int) {
-        self.ID = UUID()
-        self.plantName = plantName
-        self.plantType = plantType
-        self.minTemperature = minTemperature
-        self.maxTemperature = maxTemperature
-        self.minHumidity = minHumidity
-        self.maxHumidity = maxHumidity
-        self.minPrecipitation = minPrecipitation
-        self.maxPrecipitation = maxPrecipitation
-        self.minUV = minUV
-        self.maxUV = maxUV
-    }
 }
