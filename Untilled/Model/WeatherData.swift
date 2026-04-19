6282f10bf239968330507b219640396c76700673
Mark Lorens <marklorens0705@gmail.com>
2026-04-19 16:34:45 +0800

    Adjusted file structures.

--- Untilled/Model/WeatherData.swift
+++ Untilled/Model/WeatherData.swift
@@ -7,17 +7,6 @@
 import SwiftData
 import Foundation
 
-struct OpenMeteoResponse: Decodable {
-    struct Current: Decodable {
-        let temperature_2m: Double
-        let relative_humidity_2m: Double
-        let precipitation: Double
-        let uv_index: Double
-    }
-    
-    let current: Current
-}
-
 @Model
 class WeatherData{
     var temperature: Double
@@ -31,33 +20,4 @@ class WeatherData{
         self.precipitation = precipitation
         self.uv = uv
     }
-}
-
-extension WeatherData {
-    static func from(response: OpenMeteoResponse) -> WeatherData {
-        return WeatherData(
-            temperature: response.current.temperature_2m,
-            humidity: response.current.relative_humidity_2m,
-            precipitation: response.current.precipitation,
-            uv: response.current.uv_index
-        )
-    }
-}
-
-// Open-Meteo Fetch
-
-func fetchWeather() async throws -> WeatherData {
-    let urlString = """
-    https://api.open-meteo.com/v1/forecast?latitude=-8.65&longitude=115.22&current=temperature_2m,relative_humidity_2m,precipitation,uv_index
-    """
-    
-    guard let url = URL(string: urlString) else {
-        throw URLError(.badURL)
-    }
-
-    let (data, _) = try await URLSession.shared.data(from: url)
-    
-    let decoded = try JSONDecoder().decode(OpenMeteoResponse.self, from: data)
-    
-    return WeatherData.from(response: decoded)
 }
--- Untilled/Views/Screens/MainScreen.swift
+++ Untilled/Views/Screens/MainScreen.swift
@@ -7,17 +7,6 @@
 
 import SwiftUI
 
-var plantDatas: [PlantData] = [
-    PlantData(plantName: "Monstera", plantType: "Leaves", minTemperature: 20.1, maxTemperature: 25.2, minHumidity: 10.0, maxHumidity: 90.0, minPrecipitation: 0.0, maxPrecipitation: 10.0, minUV: 1, maxUV: 6),
-    PlantData(plantName: "Cactus", plantType: "Cactus", minTemperature: 20.1, maxTemperature: 25.2, minHumidity: 10.0, maxHumidity: 90.0, minPrecipitation: 0.0, maxPrecipitation: 10.0, minUV: 1, maxUV: 6),
-    PlantData(plantName: "Rose", plantType: "Rose", minTemperature: 20.1, maxTemperature: 25.2, minHumidity: 10.0, maxHumidity: 90.0, minPrecipitation: 0.0, maxPrecipitation: 10.0, minUV: 1, maxUV: 6),
-    PlantData(plantName: "Jasmine", plantType: "Jasmine", minTemperature: 20.1, maxTemperature: 25.2, minHumidity: 10.0, maxHumidity: 90.0, minPrecipitation: 0.0, maxPrecipitation: 10.0, minUV: 1, maxUV: 6),
-    PlantData(plantName: "Peach", plantType: "Fruity", minTemperature: 20.1, maxTemperature: 25.2, minHumidity: 10.0, maxHumidity: 90.0, minPrecipitation: 0.0, maxPrecipitation: 10.0, minUV: 1, maxUV: 6),
-    PlantData(plantName: "Tulip", plantType: "Tulip", minTemperature: 20.1, maxTemperature: 25.2, minHumidity: 10.0, maxHumidity: 90.0, minPrecipitation: 0.0, maxPrecipitation: 10.0, minUV: 1, maxUV: 6),
-    PlantData(plantName: "Sunflower", plantType: "Sunflower", minTemperature: 20.1, maxTemperature: 25.2, minHumidity: 10.0, maxHumidity: 90.0, minPrecipitation: 0.0, maxPrecipitation: 10.0, minUV: 1, maxUV: 6),
-    PlantData(plantName: "Orchid", plantType: "Orchid", minTemperature: 20.1, maxTemperature: 25.2, minHumidity: 10.0, maxHumidity: 90.0, minPrecipitation: 0.0, maxPrecipitation: 10.0, minUV: 1, maxUV: 6),
-    PlantData(plantName: "Grape", plantType: "Grape", minTemperature: 20.1, maxTemperature: 25.2, minHumidity: 10.0, maxHumidity: 90.0, minPrecipitation: 0.0, maxPrecipitation: 10.0, minUV: 1, maxUV: 6),
-]
 struct MainScreen: View {
     let columns: [GridItem] = [
         GridItem(.flexible()),
@@ -30,13 +19,13 @@ struct MainScreen: View {
             ScrollView(.vertical, showsIndicators: false){
                 VStack(alignment: .leading){
                     MainLogo()
-                    
-                    //                if let weather {
-                    //                    WeatherOverviewCard(data: weather)
-                    //                } else {
-                    //                    ProgressView()
-                    //                        .padding()
-                    //                }
+
+    //                if let weather {
+    //                    WeatherOverviewCard(data: weather)
+    //                } else {
+    //                    ProgressView()
+    //                        .padding()
+    //                }
                     HStack{
                         Text("My Plants")
                             .font(.title3.bold())
