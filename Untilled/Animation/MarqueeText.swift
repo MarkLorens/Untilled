19a7f535d130910fe0bf8c4f76dabd2c4657cf14
Mark Lorens <marklorens0705@gmail.com>
2026-04-23 14:50:40 +0800

    Moving text for plant details.

--- Untilled/Animation/MarqueeText.swift
+++ Untilled/Animation/MarqueeText.swift
@@ -9,50 +9,53 @@ import SwiftUI
 
 struct MarqueeText: View {
     let text: String
-    let font: Font = .body
-    var duration: Double = 10.0
+    var duration: Double = 15.0
     var delay: Double = 1.0
-    var threshod: Int = 8
+    var threshold: Int = 8
     
     @State private var textSize: CGSize = .zero
     @State private var animate: Bool = false
+    
     var body: some View {
         GeometryReader { geo in
             let containerWidth = geo.size.width
-            
-            ZStack{
-                HStack{
-                    Text(text)
-                        .background(
-                            GeometryReader { textGeo in
-                                Color.clear
-                                    .onAppear{
-                                        textSize = textGeo.size
-                                        animate = true
-                                    }
-                            }
-                        )
-                        .fixedSize()
-                    
-                    Text(text)
-                        .fixedSize()
+    
+            if (text.count > threshold){
+                ZStack{
+                    HStack(spacing: 30){
+                        Text(text)
+                            .background(
+                                GeometryReader { textGeo in
+                                    Color.clear
+                                        .onAppear{
+                                            textSize = textGeo.size
+                                            animate = true
+                                        }
+                                }
+                            )
+                            .fixedSize()
+                        
+                        Text(text)
+                            .fixedSize()
+                    }
+                    .offset(x: animate ? -textSize.width - 30 : 0)
+                    .animation(
+                        animate
+                        ? Animation.linear(duration: duration)
+                            .delay(delay)
+                            .repeatForever(autoreverses: false)
+                        : .default, value: animate
+                    )
                 }
-                .offset(x: animate ? -textSize.width - 30 : 0)
-                .animation(
-                    animate
-                    ? Animation.linear(duration: duration)
-                        .delay(delay)
-                        .repeatForever(autoreverses: false)
-                    : .default, value: animate
-                )
+                .frame(width: containerWidth, alignment: .leading)
+                .clipped()
+            } else {
+                Text(text)
+                    .lineLimit(1)
+                    .truncationMode(.tail)
+                    .frame(width: containerWidth, alignment: .leading)
             }
-            .frame(width: containerWidth, alignment: .leading)
-            .clipped()
         }
         .frame(height: 50)
     }
-}
-
-#Preview {
-    MarqueeText(text: "Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World")
 }
